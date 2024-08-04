9adced9690b20322d80d144fb341e021040329ce
denizmgun <denizmgun@gmail.com>
2024-08-04 12:59:13 +0200

    Added stochastic uniform sampling surrounding the selected conditions

--- src/autora/experimentalist/autora_experimentalist_max_min_distance/__init__.py
+++ src/autora/experimentalist/autora_experimentalist_max_min_distance/__init__.py
@@ -4,9 +4,10 @@ It chooses the next sample based on the maximal minimal difference to a comparis
 If outperforms random sampling for low sampling steps on synthetic experimental data.
 It is a custom implementation of the "novelty" approach from autorRA's novelty experimentalist.
 Unlike autoRA's novelty experimentalist, this one only offers the calculation of the Euclidean distance and always
-utilizes the maximal _min_ distance to choose the next point.
-Furthermore, dynamic resampling of the condition space to improve performance is provided.
-"""
+utilizes the maximal _min_ distance to choose the next point. Once a suitable condition is chosen, it is possible
+to add a small stochastic element to it by sampling uniformly from a n-dimensional blob surrounding that point.
+Blob size is chosen such that there is no overlap for candidate point and the condition space is optimally covered.
+Furthermore, dynamic resampling of the condition space to improve performance is provided."""
 
 import numpy as np
 import pandas as pd
@@ -15,7 +16,55 @@ import pandas as pd
 candidate_points are all points in the conditions space that could potentially be proposed.
 It is a result of resampling the entirety of condition space. Must be reinitialized before each experiment.
 """
+# don't like using globals but the challenge set up requires it.
 candidate_points = None
+"""The size of the uniform random blobs surrounding selected candidate points. (Suggested conditions)"""
+blob_scale = np.array([0])
+
+
+def uniform_blob(center, num_samples=1):
+    """
+    Generates samples from an n-dimensional uniform probability distribution centered at 'center'.
+
+    Args:
+        center (np.ndarray): The center of the uniform blob. A point in n-dimensional space
+        num_samples (int):   Number of samples to generate.
+
+    Returns:
+        np.ndarray: Generated samples.
+    """
+    global blob_scale
+
+    n_dims = len(center)
+    # Compute blob low and high points and clip them to the boundaries of candidate_points
+    dim = len(center)
+    low = np.maximum(center-blob_scale, np.zeros(dim))
+    high = np.minimum(center+blob_scale, np.ones(dim) * range_scale)
+
+    return np.random.uniform(low=low, high=high, size=(num_samples, dim))
+
+
+def compute_optimal_blob_size(space):
+    """
+
+    Args:
+        space (DataFrame) : A condition space. For this use cases expected to be candidate_points
+
+    Returns:
+        The optimal size for blobs centered around candiate_points to cover the entire condition space
+        without overlap
+    """
+
+    # Compute a range such that uniform blobs of candidate points do not overlap but all together
+    # cover the entire condition_space
+    range_scale = []
+    n_dims = len(space.columns)
+
+    for col in space.columns:
+        col_range = candidate_points[col].max()-candidate_points[col].min()
+        range_scale.append(col_range / n_dims)
+
+    return np.array(range_scale)
 
 
 def euclidean_distance(point1, point2):
@@ -77,31 +126,30 @@ def check_resampling_required(condition_space, candidate_points, values_per_dim)
     Returns:
         bool True if resampling is required
     """
-
     # 1. If there are no candidate_points
     if candidate_points is None:
         return True
-
     # 2. If candidate_points.shape[0] != values_per_dim**n where n is the number of columns in condition_space
     if candidate_points.shape[0] != values_per_dim ** len(condition_space.columns):
         return True
-
     # 3. If independent variable names have changed
     if not (candidate_points.columns == condition_space.columns):
         return True
-
     # 4. If the boundaries of the condition_space have changed
     for col in condition_space.columns:
         if condition_space[col].min() != candidate_points[col].min() or \
            condition_space[col].max() != candidate_points[col].max():
-
             return True
 
     # No resampling required
     return False
 
 
-def max_min_distance_sampler(data, condition_space, n_samples, values_per_dim=10):
+def max_min_distance_sampler(data,
+                             condition_space,
+                             n_samples,
+                             values_per_dim=10,
+                             stochastic=False):
     """
     Picks the next set of experimental conditions by maximizing the
     distance to all previously recorded conditions. Default distance measure
@@ -116,6 +164,9 @@ def max_min_distance_sampler(data, condition_space, n_samples, values_per_dim=10
         n_samples (int):              Number of samples to return per step
         values_per_dim (int):         The number of discrete values each independent variable can assume
                                       after resampling.
+        stochastic (bool):            If true, on top of max min-distance selection a small stochastic
+                                      displacement is added.
+
     Returns:
         conditions (DataFrame):       DataFrame featuring next proposed experimental
                                       conditions
@@ -136,11 +187,13 @@ def max_min_distance_sampler(data, condition_space, n_samples, values_per_dim=10
     # Class level variable, since autora_experimentalists do not seem to be instance based
     # TODO: Discuss use case and change in the future.
     global candidate_points
+    global blob_scale
 
     # Decide whether candidate_points must be resampled
     if check_resampling_required(condition_space, candidate_points, values_per_dim):
         # Resample the condition space to create a dense grid of candidate points.
         candidate_points = resample_condition_space(condition_space, values_per_dim)
+        compute_optimal_blob_size()
 
     # Identify which conditions have already been sampled before.
     previous_conditions = data[condition_space.columns].values
@@ -149,6 +202,10 @@ def max_min_distance_sampler(data, condition_space, n_samples, values_per_dim=10
     for _ in range(n_samples):
         # Find new condition based on max_min_distance to previous conditions
         next_condition = max_min_distance_selection(candidate_points, previous_conditions)
+
+        if stochastic:
+            next_condition = uniform_blob(next_condition)
+
         new_conditions.append(next_condition)
 
         # Insert new condition into previous_conditions
